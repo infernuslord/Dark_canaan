1dbd8b97c3fa4dc1cd1968d389c024943edbeed2
infernuslord <masterchaos.lord@gmail.com>
2015-01-03 04:48:26 +0100

    comtools compiles aggmemb.h		1.12 comconn.h		1.2 comtools.h		1.55 constrid.h		1.1 genagg.h		1.11 geniset.h		1.10 interset.h		1.7 objcguid.h		1.1 objcoll.h		1.20 pguidset.h		1.5 pintarr.h		1.3
    comconn.cpp		1.2
    comtools.cpp	1.3
    genagg.cpp		1.24
    geniset.cpp		1.14
    interset.cpp	1.10
    pguidset.cpp	1.8
    pintarr.cpp		1.6

---
+++ tech/libsrc/comtools/pintarr.cpp
@@ -0,0 +1,12 @@
+///////////////////////////////////////////////////////////////////////////////
+// $Source: x:/prj/tech/libsrc/comtools/RCS/pintarr.cpp $
+// $Author: TOML $
+// $Date: 1997/07/03 13:25:19 $
+// $Revision: 1.6 $
+//
+
+//#include <windows.h>
+#include <lg.h>
+#include <comtools.h>
+
+#include <pintarr.h>
