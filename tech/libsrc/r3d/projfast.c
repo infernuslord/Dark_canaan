61840b68aed5d3b3cb3339e1957a10a4a424dff8
infernuslord <masterchaos.lord@gmail.com>
2015-01-24 00:24:32 +0100

    r3d finished
    projfast.asm	1.8
    
    block.c			1.5
    clip.c			-
    clipclip.c		1.9
    clipglob.c		1.24
    clipplan.c		1.7
    clipproj.c		1.8
    cliptest.c		1.4
    clipunsc.c		1.6
    ctxtguts.c		1.26
    ctxtmang.c		1.9
    ctxtmult.c		1.6
    frame.c			1.3
    init.c			1.9
    object.c		1.14
    polytest.c		-
    prim.c			1.12
    primconv.c		1.12
    primd3d.c		1.7
    primface.c		1.7
    primfunc.c		1.22
    primg2.c		1.6
    primmisc.c		1.11
    primnull.c		1.2
    primpoly.c		1.17
    projfast.c		-
    space.c			1.38
    utils.c			1.3
    view.c			1.23
    xfrmclip.c		1.7
    xfrmglob.c		1.5
    xfrmlin.c		1.7
    xfrmproj.c		1.9
    xfrmunsc.c		1.7
    
    block.h			-
    clip.h			1.13
    clipcon.h		1.8
    clipcon_.h		1.3
    clipoff.h		-
    clipplan.h		1.3
    clipprim.h		1.10
    cliptab.h		-
    clipu.h			-
    clip_.h			1.3
    ctxtguts.h		1.1
    ctxts.h			1.14
    grnull.h		1.2
    init.h			_
    object.h		1.6
    prim.h			1.12
    primcon.h		1.5
    primconv.h		1.6
    primcon_.h		1.4
    primface.h		1.4
    primfunc.h		1.6
    primpoly.h		1.5
    primtab.h		1.7
    primtab_.h		1.5
    r3d.h			-
    r3dctxt.h		1.7
    r3ds.h			1.13
    r3dutil.h		1.4
    space.h			1.15
    utils.h			1.3
    view.h			1.7
    xform.h			1.9
    xfrmtab.h		1.6
    _ctxt.h			1.5
    _test.h			1.2
    _xfrm.h			1.14

---
+++ tech/libsrc/r3d/projfast.c
@@ -0,0 +1,17 @@
+// NON-LG: Rewrite of .asm file to C. Partially generated
+// Original file name: projfast.asm
+
+#include <assert.h>
+
+void r3_transform_block_proj_noclip(int n, r3s_point *dest, mxs_vector *src, mxs_trans *t)
+{
+    // STUB: Needs code
+    assert(0);
+}
+
+void r3_transform_block_proj_clip(int n, r3s_point *dest, mxs_vector *src, mxs_trans *t)
+{
+    // STUB: Needs code
+    assert(0);
+}
+
